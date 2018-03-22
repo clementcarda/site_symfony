eb34189cab28021b5d860320ff38a3b25b52db4d
clementcarda <clement.cardarelli@ynov.com>
2018-03-22 11:27:08 +0100

    add FOSUser

---
+++ src/AppBundle/Entity/User.php
@@ -0,0 +1,28 @@
+<?php
+namespace AppBundle\Entity;
+
+use FOS\UserBundle\Model\User as BaseUser;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="users")
+ */
+class User extends BaseUser
+{
+    /**
+     * @ORM\Id
+     * @ORM\Column(type="integer")
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    protected $id;
+
+    /**
+     * get id
+     *
+     *@return integer
+     */
+    public function getId(){
+        return $this->id;
+    }
+}
